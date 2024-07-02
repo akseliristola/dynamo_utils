1466386e3f0745fdd7af7af8ba8ecba1317cbaf0
akseliristola <akseli.ristola@gmail.com>
2024-07-02 13:08:49 +0300

    x

--- utils.js
+++ utils.js
@@ -31,54 +31,34 @@ function parseDynamoObj(item) {
 }
 
 
-
-  function objectToDynamodb(item) {
-    if(Array.isArray(item)){
-      const marshalled = {L:item.map(v=>objectToDynamodb(v))}
-      return marshalled
-    }
-    else if(typeof item==="string"){
-      const marshalled = {S:item}
-      return marshalled
-    }
-    else if(typeof item==="number"){
-      const marshalled = {N:item.toString()}
-      return marshalled
-    }
-    else if(typeof item==="boolean"){
-      const marshalled = {BOOL:item}
-      return marshalled
-    }
-    else if(item===null){
-      const marshalled = {NULL:true}
-      return marshalled
-    }
-
-
-    function marshall(item){
-      const marshalled = {};
-      for (const key in item) {
-        const value = item[key];
-        if (typeof value === 'string') {
-          marshalled[key] = {S: value };
-        } else if (typeof value === 'number') {
-          marshalled[key] = {N: value.toString() };
-        } else if (typeof value === 'boolean') {
-          marshalled[key] = {BOOL: value };
-        } else if (Array.isArray(value)) {
-          marshalled[key] = {L: value.map((v) => objectToDynamodb(v)) };
-        } else if (value === null) {
-          marshalled[key] = {NULL: true };
-        } else if (typeof value === 'object') {
-          marshalled[key] = marshall(value);
-        }
-      }
-      return marshalled;
-    }
-
-    return marshall({M:item})
-
+function objectToDynamodb(item) {
+  if (item === null) {
+    return { NULL: true };
   }
+
+  switch (typeof item) {
+    case 'string':
+      return { S: item };
+    case 'number':
+      return { N: item.toString() };
+    case 'boolean':
+      return { BOOL: item };
+    case 'object':
+      if (Array.isArray(item)) {
+        return { L: item.map(objectToDynamodb) };
+      } else {
+        const marshalled = {};
+        for (const [key, value] of Object.entries(item)) {
+          marshalled[key] = objectToDynamodb(value);
+        }
+        return { M: marshalled };
+      }
+    default:
+      throw new Error(`Unsupported type: ${typeof item}`);
+  }
+}
+
+
 const aa={parseDynamoObj,objectToDynamodb}
 
 module.exports=aa
